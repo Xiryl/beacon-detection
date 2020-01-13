c0d27130acfc2fb3853f8f90ab7f16d7923e1d8b
Fabio <fabio@chiarani.it>
2020-01-13 18:48:45 +0100

    first build

--- droid/app/src/androidTest/java/it/chiarani/beacon_detection/ExampleInstrumentedTest.java
+++ droid/app/src/androidTest/java/it/chiarani/beacon_detection/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package it.chiarani.beacon_detection;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
